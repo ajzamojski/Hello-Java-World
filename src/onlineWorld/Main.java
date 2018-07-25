3a9abb906b1a30ed012d1dd3ab124d065df3814f
Adam Zamojski <ajzamojski@gmail.com>
2018-07-24 21:17:04 -0400

    first commit

---
+++ src/onlineWorld/Main.java
@@ -0,0 +1,30 @@
+package onlineWorld;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+//		System.out.println("Hello, World");
+		
+//		for (int i = 0; i < 100; i++) {
+//			
+//			System.out.println(i);
+//			
+//		}
+//		int a = 10;
+//		do {
+//			
+//			System.out.println(a);
+//			
+//		} while (a == 10);
+//		
+//	}
+		
+		String message = "Hello World!";
+		
+		message.concat("lovely day, isn't ist?");
+		
+		System.out.println(message);
+		
+	}
+}
